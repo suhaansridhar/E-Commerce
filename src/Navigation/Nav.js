780bd37fef52275e92a97815df67b8063be1520f
suhaan sridhar <suhaansridhar@suhaans-MacBook-Pro.local>
2023-09-05 20:55:59 +0100

    updated nav page

--- src/Navigation/Nav.js
+++ src/Navigation/Nav.js
@@ -1,19 +1,20 @@
+import {AiOutlineHeart, AiOutlineShoppingCart, AiOutlineUser} from 'react-icons/ai';
 import './Nav.css';
-import {AiOutlineHeart, AiOutlineShoppingCart, AiOutlineSearch} from 'react-icons/ai';
-
-export default function Nav() {
-  return (
+function Nav(){
+  return(
     <div className="nav--container--outer">
       <div className="nav--container--inner">
         <div>
-          <input type="text" placeholder="Search for your shoes"/>
+          <input type="text" placeholder="Enter your shoe"/>
         </div>
-        <div className='nav--container--icons'>
+        <div className="nav--container--inner--icons">
           <AiOutlineHeart className='nav--container--icons--items'/>
-          <AiOutlineSearch className='nav--container--icons--items'/>
           <AiOutlineShoppingCart className='nav--container--icons--items'/>
+          <AiOutlineUser className='nav--container--icons--items'/>
         </div>
       </div>
     </div>
   )
 }
+
+export default Nav;
