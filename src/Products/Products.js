40afb3f44873b5c471f04509071eb3022fc6c4a5
suhaan sridhar <suhaansridhar@suhaans-MacBook-Pro.local>
2023-09-05 21:11:06 +0100

    updated products page

--- src/Products/Products.js
+++ src/Products/Products.js
@@ -1,28 +1,25 @@
+import {AiOutlineStar} from 'react-icons/ai';
+import {BsFillBagHeartFill} from 'react-icons/bs';
 import './Products.css';
 
-import React from 'react'
-import { AiFillStar } from 'react-icons/ai';
-import {BsFillBagFill} from 'react-icons/bs';
-
-export default function Products() {
-  return (
-    <div className='products--container'>
-      <section className="card">
-        <img src='https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg'
-          alt='shoe'/>
-        <div className="card-details">
-          <h3 className="card-title">Shoe</h3>
-          <section className="card-reviews">
-            <AiFillStar />
-            <AiFillStar />
-            <AiFillStar />
-            <AiFillStar />
-          </section>
-          <del>300</del>200
-          <BsFillBagFill />
-        </div>
+function Products(){
+  return(
+    <div className="products--container">
+      <section className="products--container--image">
+        <img src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+          alt="show image" />
       </section>
-      
+      <section className="product--container--name">Shoe</section>
+      <section className="product--container--stars">
+        <AiOutlineStar className='product--container--stars--item'/>
+        <AiOutlineStar className='product--container--stars--item'/>
+        <AiOutlineStar className='product--container--stars--item'/>
+        <AiOutlineStar className='product--container--stars--item'/>
+      </section>
+      <section className="product--container--price"><del>300</del>200</section>
+      <section className="product--container--bag"><BsFillBagHeartFill /></section>
     </div>
   )
 }
+
+export default Products;
