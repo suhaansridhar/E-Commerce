4c53f9335879f48bf29bbb0d60566a1b704a28b7
suhaan sridhar <suhaansridhar@suhaans-MacBook-Pro.local>
2023-09-06 18:21:56 +0100

    edited

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import Sidebar from './Sidebar/Sidebar.js';
 
 function App(){
   return(
-    <div>
-      <Sidebar />
-      <Nav />
-      <Recommended /> <br />
-      <Products />
+    <div className='home--container'>
+      <div className='home--container--left--side'>
+        <Sidebar />
+      </div>
+      <div className='home--container--right--side'>
+        <Nav />
+        <Recommended /> <br />
+        <Products />
+      </div>
     </div>
   )
 }
